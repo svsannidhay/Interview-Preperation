84ef880c23222aeec39bcc5e3f0d5bb425176f4b
svsannidhay <sannidhayvashalsv@gmail.com>
2021-07-07 16:40:46 +0530

    Modified problem 42

--- Love Babbar DSA sheet/Matrix/42. maximum area rectangle.cpp
+++ Love Babbar DSA sheet/Matrix/42. maximum area rectangle.cpp
@@ -1,3 +1,5 @@
+#define ll long long int
+
 vector<ll> nsel(vector<ll> arr, ll n)
 {
   stack<pair<ll, ll>> st;
@@ -17,7 +19,7 @@ vector<ll> nsel(vector<ll> arr, ll n)
 vector<ll> nser(vector<ll> arr, ll n)
 {
   stack<pair<ll, ll>> st;
-  vector<ll> ans(n, -1);
+  vector<ll> ans(n, n);
   for (ll i = 0; i < arr.size(); i++)
   {
     while (!st.empty() && st.top().first > arr[i])
@@ -30,16 +32,63 @@ vector<ll> nser(vector<ll> arr, ll n)
   return ans;
 }
 
-ll maximum_area_histogram(vector<ll> arr, n)
+ll maximum_area_histogram(vector<ll> arr, ll n)
 {
   vector<ll> nsel_v = nsel(arr, n);
   vector<ll> nser_v = nser(arr, n);
+
+  // for(auto i : nsel_v) cout<<i<<" ";
+  // cout<<"\n";
+
+  // for(auto i : nser_v) cout<<i<<" ";
+  // cout<<"\n";
 
   ll ans = 0;
   for (ll i = 0; i < n; i++)
   {
     ans = max(ans, arr[i] * (nser_v[i] - nsel_v[i] - 1));
+    // cout<<ans<<"\n";
   }
 
   return ans;
 }
+
+class Solution
+{
+public:
+  int maxArea(int M[MAX][MAX], int n, int m)
+  {
+    int mat[n + 1][m + 1];
+    for (ll i = 0; i < m; i++)
+    {
+      ll val = 0;
+      for (ll j = 0; j < n; j++)
+      {
+        if (M[j][i] == 0)
+        {
+          val = 0;
+        }
+        else
+        {
+          val += 1;
+        }
+        mat[j][i] = val;
+      }
+    }
+
+    ll ans = 0;
+
+    for (ll i = 0; i < n; i++)
+    {
+      vector<ll> arr;
+      for (ll j = 0; j < m; j++)
+      {
+        arr.push_back(mat[i][j]);
+        // cout<<mat[i][j]<<" ";
+      }
+      // cout<<"\n";
+      ans = max(ans, maximum_area_histogram(arr, m));
+    }
+    return ans;
+  }
+};
