591c663cc3c0221a61bb9dc7a8e6b1cfc5825b86
svsannidhay <sannidhayvashalsv@gmail.com>
2021-07-22 15:45:26 +0530

    Standard DP problems covered starting graph algorithms

--- DSA /Dynamic programming/Dp on trees/2. Maximum sum path from any node to any node in a binary tree.cpp
+++ DSA /Dynamic programming/Dp on trees/2. Maximum sum path from any node to any node in a binary tree.cpp
@@ -1,20 +1,26 @@
-class Solution {
+class Solution
+{
 public:
-    int solve(TreeNode * root,int &res) {
-        if(root == NULL) return 0;
-        int l = solve(root->left,res);
-        int r = solve(root->right,res);
-        
-        if(res == INT_MIN) res = max(l + r + root->val,max(root->val,max(l + root->val, r + root->val)));
-        else res = max(res, max(l + r + root->val,max(root->val,max(l + root->val, r + root->val)))  );
-        
-        return max(root->val, root -> val + (max(l , r)));
-        
-    }
-    
-    int maxPathSum(TreeNode* root) {
-        int res = INT_MIN;
-        solve(root,res);
-        return res;
-    }
+  int solve(TreeNode *root, int &res)
+  {
+    if (root == NULL)
+      return 0;
+    int l = solve(root->left, res);
+    int r = solve(root->right, res);
+
+    int temp = max(root->val, root->val + max(l, r));
+
+    int ans = max(temp, root->val + l + r);
+
+    res = max(res, ans);
+
+    return temp;
+  }
+
+  int maxPathSum(TreeNode *root)
+  {
+    int res = INT_MIN;
+    solve(root, res);
+    return res;
+  }
 };
---
+++ DSA /Graph Algorithms/Depth first search.cpp
@@ -0,0 +1,88 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <map>
+#include <math.h>
+#include <climits>
+#include <set>
+#include <cstring>
+#include <unordered_map>
+#include <cstdlib>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <bitset>
+#include <stdlib.h>
+#include <chrono>
+ 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#define fio  ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ll  int
+#define ull unsigned long long int
+#define cinll(x) ll x;cin >> x;
+#define cini(x) int x;cin >> x;
+#define cins(x) string x;cin >> x;
+#define vect(x) vector<ll> x
+#define vect1(x) vector<ll> x;x.push_back(0);
+#define pb(x) push_back(x)
+#define mp(x, y) make_pair(x, y)
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#define MAX 1e17
+#define MIN -9223372036854775800
+#define MOD 1000000007
+#define f first
+#define s second
+#define LOG 32
+#define MAX_S 100001
+const ll maxN = 17;
+///////////////////////////////////////////////////////////////////////////////////////////////////
+using namespace std;
+using u64 = uint64_t;
+
+void addEdge(vector< ll > adj[],ll u,ll v) {
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+    return;    
+}
+
+void dfs(vector< ll > adj[],ll current,vector<bool> &visited) {
+    if(!visited[current]) {
+        visited[current] = true;
+        cout<<current<<" ";
+        for(ll i = 0;i < adj[current].size(); i++) {
+            dfs(adj,adj[current][i],visited);
+        }
+    } 
+}
+
+void solve() {
+    cinll(n);cinll(m);
+    vector<ll> adj[n+1];
+    for(ll i = 0;i < m;i++) {
+        cinll(u);cinll(v);
+        addEdge(adj,u,v);
+    }
+    vector<bool> visited(n,false);
+    dfs(adj,1,visited);
+    return;
+}
+
+
+int main() {
+    fio;
+///////////////////////////////////////////    
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt" , "r", stdin);
+    freopen("output.txt", "w" , stdout);
+    #endif
+// ///////////////////////////////////////////
+    cinll(t);
+    for(ll i=0;i<t;i++) {
+        solve();
+    }
+  return 0;
+}
