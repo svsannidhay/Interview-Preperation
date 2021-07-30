66f834569c5957821fd3746016fdc35cc5a8d431
svsannidhay <sannidhayvashalsv@gmail.com>
2021-07-30 22:49:57 +0530

    Added diagonal traversal of a binary tree

---
+++ Love Babbar DSA sheet/Binary tree/15. Diagonal Traversal of Binary tree.cpp
@@ -0,0 +1,19 @@
+void dfs(Node *root,int pos,vector<int> adj[]) {
+    if(root == NULL) return;
+    adj[pos].push_back(root->data);
+    if(root->left != NULL) dfs (root->left,pos+1,adj);
+    if(root->right != NULL) dfs(root->right,pos,adj);
+    return;
+}
+
+vector<int> diagonal(Node *root) {
+    vector<int> adj[100005];
+    dfs(root,0,adj);
+    vector<int> ans;
+    for(int i = 0; i< 100000; i++) {
+        for(int j = 0;j < adj[i].size(); j++) {
+            ans.push_back(adj[i][j]);
+        }
+    }
+    return ans;
+}
