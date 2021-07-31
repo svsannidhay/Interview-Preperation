83dd9a0c5f5b699c7f134dbca8ca30f635b4f8d6
svsannidhay <sannidhayvashalsv@gmail.com>
2021-07-31 16:17:35 +0530

    Added binary tree to DLL

---
+++ Love Babbar DSA sheet/Binary tree/18. Binary Tree to DLL.cpp
@@ -0,0 +1,39 @@
+/*
+
+  Use preorder traversal and keep the track of preveiously visited node 
+
+  Time Complexity: O(N)
+  Space Complexity: O(N)
+
+*/
+class Solution
+{
+    public: 
+    
+    void dfs(Node *root,Node **prev,Node **head) {
+        if(root == NULL) return;
+        dfs(root->left,prev,head);
+        Node * curr = new Node;
+        curr->data = root->data;
+        curr->left = curr->right = NULL;
+        if(*prev != NULL) {
+            curr->left = *prev;
+            (*prev)->right = curr;
+        } else {
+            *head = curr;
+        }
+        *prev = curr;
+        dfs(root->right,prev,head);
+        return;
+    }
+    
+    //Function to convert binary tree to doubly linked list and return it.
+    Node * bToDLL(Node *root)
+    {
+        
+        Node *head = NULL;
+        Node *prev = NULL;
+        dfs(root,&prev,&head);
+        return head;
+    }
+};
