f8fe02b42af3c3408ef1d78130164b05386d6409
svsannidhay <sannidhayvashalsv@gmail.com>
2021-08-05 18:56:27 +0530

    Added Add 1 to a number represented as a linked list

---
+++ Love Babbar DSA sheet/LinkedList/9. Add 1 to a number represented as a linked list.cpp
@@ -0,0 +1,38 @@
+/*
+
+    Logic : Find the last non 9 position add 1 to it and after it make everything 0
+    if(there is no non 9 position) insert 1 at the begening of the list 
+
+*/
+
+class Solution
+{
+    public:
+    Node* addOne(Node *head) {
+        Node * last_non_nine = NULL;
+        Node * move = head;
+        while(move != NULL) {
+            if(move->data != 9) last_non_nine = move;
+            move = move->next;
+        }
+        
+        if(last_non_nine == NULL) {
+            Node * start = head;
+            while(start != NULL) {
+                start->data = 0;
+                start = start->next;
+            }
+            Node * one = new Node(1);
+            one->next = head;
+            head = one;
+            return head;
+        }
+        last_non_nine->data++;
+        last_non_nine = last_non_nine->next;
+        while(last_non_nine != NULL) {
+            last_non_nine->data = 0;
+            last_non_nine = last_non_nine->next;
+        }
+        return head;
+    }
+};
